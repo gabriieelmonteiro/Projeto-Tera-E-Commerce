1e3024c07d1500fa433011c9e814e5508c97c1e0
[Gabriel <118690659+gabriieelmonteiro@users.noreply.github.com>
2023-04-19 22:49:34 -0300

    desenvolvimento pagina principal

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  pagination: {
+  el: '.swiper-pagination',
+  type: 'bullets',
+  }
+  autoplay: {
+    delay: 5000,
+});
