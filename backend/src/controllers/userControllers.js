c87667ece8fff341d4dfd005a4260e122a1f690b
Felipe Ramos Braidotti <braidottifelipe@hotmail.com>
2023-06-13 20:10:34 -0300

    criação do método POST para criar usuários no banco de dados

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -3,12 +3,53 @@ import UserSchema from "../models/userSchema.js";
 // O CRUD será montado neste arquivo: userControllers
 
 // Listar todos os usuários da Database
-const getAll = (req, res) => {
-  console.log("Bateu na rota");
+const getAll = async (req, res) => {
+  try {
+    const users = await UserSchema.find();
+    if (!users) {
+      res.status(500).send({
+        statusCode: 500,
+        message: err.message,
+      });
+    }
+    res.status(200).send({
+      status: 200,
+      data: {
+        users,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 // Criar novo usuário e registrar na Database
+const createUser = async (req, res) => {
+  // Acessar as informações do usuário que vem do frontend
+  // Acessar a partir do body da requisição
+  try {
+    // Criar um novo usuário
+    const newUser = new UserSchema(req.body);
+    console.log(newUser);
+    // Salva esse usuário
+    const savedUser = await newUser.save();
+    // Enviar resposta de "usuário criado com sucesso"
+    res.status(201).send({
+      statusCode: 201,
+      data: {
+        savedUser,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({
+      statusCode: 500,
+      message: "User not saved",
+    });
+  }
+};
 
 export default {
   getAll,
+  createUser,
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import controllers from "../controllers/userControllers.js";
 
 router.get("/", controllers.getAll);
+router.post("/", controllers.createUser);
 
 export default router;
