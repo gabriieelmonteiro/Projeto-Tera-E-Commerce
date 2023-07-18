2f9ef6ba891dfd986b84e85bdee1ab4985f00c7b
Felipe Ramos Braidotti <braidottifelipe@hotmail.com>
2023-07-18 20:28:07 -0300

    teste

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 // MODULE E EXPORTS
 // todo arquivo com código em JavaScript é considerado um módulo
-// Arquivo responsáve por armazenar as informações pricipais da API
+// Arquivo responsável por armazenar as informações pricipais da API
 
 import express from "express"; // importando o módulo express (módulo externo instalado com o pacote npm)
 // instanciando o EXPRESS > instanciar: deixar disponível para outros módulos
@@ -10,7 +10,7 @@ import cors from "cors";
 app.use(cors()); // Boa prática de CRUD que habilita a utilização de diversos servidores
 
 // funcionalidade nativa do express para "persear" o body da requisição
-app.use(express.json()); // Utilizado para poder adotar as requisições HTTP  com HEADER e BODY em JSON.
+app.use(express.json()); // Utilizado para poder adotar as requisições HTTP com HEADER e BODY em JSON.
 
 import db from "./config/database.js"; // importando o banco de dados
 db.connect();
