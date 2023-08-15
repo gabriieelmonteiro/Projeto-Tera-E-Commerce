d846419848de5611533d4be54b8f1b6360062758
[Gabriel <118690659+gabriieelmonteiro@users.noreply.github.com>
2023-08-15 16:50:05 -0300

    implentos

--- project/src/App.js
+++ project/src/App.js
@@ -3,10 +3,13 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
 import LandingPage from './components/pages/LandingPage';
+import ObrasUser from './components/pages/ObrasUser';
 
 import "./styles/login.css"
 import "./styles/LandingPage.css"
 import "./styles/home.css"
+import "./styles/normalize.css"
+import "./styles/main.css"
 
 function App() {
   return (
@@ -15,7 +18,8 @@ function App() {
     <Route path="/" element={<Home />} />
     <Route path="/login/" element={<Login />} />
     <Route path="/landing" element={<LandingPage />} />
-    
+    <Route path="/login/:Id/post" element={<ObrasUser />} />
+    <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
     </BrowserRouter>
   );
