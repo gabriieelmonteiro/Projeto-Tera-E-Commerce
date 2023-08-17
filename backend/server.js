1e8f3dcc3b2f44a70d107e795a03215eda6e930b
Felipe Ramos Braidotti <braidottifelipe@hotmail.com>
2023-08-17 19:27:40 -0300

    removed backend part

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-// Arquivo responsável por inicializar o nosso servidor
-import app from "./src/app.js"; // importando a variáve "app" do módulo "app.js" -> deve conter o caminho da pasta onde o módulo de encontra
-
-const PORT = process.env.PORT; // definindo a porta que o servidor irá rodar
-
-// LISTEN do express: criar conexões especificando um HOST e uma PORTA
-app.listen(PORT, () => {
-  console.log(`servidor rodando na porta ${PORT}`);
-}); // sempre que o servidor estiver ativo, a mensagem acima será apresentada.
